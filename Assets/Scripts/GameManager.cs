5d46ed0bd04841c9187900cd333e481f9e5e7a36
L-and <ksw1812@naver.com>
2023-06-30 03:29:13 +0900

    start scene setting

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    // Singleton Instance에 접근하기 위한 프로퍼티
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+    public void exitApp() // 프로그램 종료
+    {
+        Application.Quit();
+    }
+
+    public void sceneMove(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
