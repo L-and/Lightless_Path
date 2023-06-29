53d8e44a7dfb86746f2467441ec38bf7b6b48e8a
pockypocky <jmt30269@gmail.com>
2023-06-30 03:07:13 +0900

    fix: unicode error

--- Assets/Scripts/CarMove.cs
+++ Assets/Scripts/CarMove.cs
@@ -7,9 +7,12 @@ public class CarMove : MonoBehaviour
     public Transform targetObject;
     public float movementSpeed = 5.0f;
     public AudioClip carClarkson;
+    public float waittime=0;
     private AudioSource audioSource;
     private Vector3 direction;
     private bool isPaused=false;
+    private bool isPaused1= false;
+    private float timer = 0;
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -20,22 +23,25 @@ public class CarMove : MonoBehaviour
         // 타겟 객체로 향하는 방향 벡터 계산
         direction = targetObject.position - transform.position;
         direction.Normalize();
-
-        // 이동 속도와 시간 간의 관계 계산
-        float distance = Vector3.Distance(transform.position, targetObject.position);
-        float moveDistance = movementSpeed * Time.deltaTime;
-        if (isPaused != true)
+        timer += Time.deltaTime;
+        if (timer > waittime)
         {
-            // 이동할 거리가 남아있는 경우에만 이동 수행
-            if (moveDistance < distance)
+            // 이동 속도와 시간 간의 관계 계산
+            float distance = Vector3.Distance(transform.position, targetObject.position);
+            float moveDistance = movementSpeed * Time.deltaTime;
+            if (isPaused != true && isPaused1 != true)
             {
-                transform.position += direction * moveDistance;
-            }
-            else
-            {
-                // 목표 위치에 도달한 경우 이동 종료
-                transform.position = targetObject.position;
-                gameObject.SetActive(false);
+                // 이동할 거리가 남아있는 경우에만 이동 수행
+                if (moveDistance < distance)
+                {
+                    transform.position += direction * moveDistance;
+                }
+                else
+                {
+                    // 목표 위치에 도달한 경우 이동 종료
+                    transform.position = targetObject.position;
+                    gameObject.SetActive(false);
+                }
             }
         }
     }
@@ -47,6 +53,10 @@ public class CarMove : MonoBehaviour
             audioSource.Play();
             Debug.Log("detect");
         }
+        if (collision.collider.gameObject.CompareTag("Car"))
+        {
+            isPaused1 = true;
+        }
     }
     private void OnCollisionStay(Collision collision)
     {
@@ -56,12 +66,22 @@ public class CarMove : MonoBehaviour
             Debug.Log("stay");
             isPaused = true;
         }
+        if (collision.collider.gameObject.CompareTag("Car"))
+        {
+            transform.position += direction * 0;
+            Debug.Log("stay");
+            isPaused1 = true;
+        }
     }
     private void OnCollisionExit(Collision collision)
     {
         if (collision.collider.gameObject.CompareTag("Player"))
         {
             isPaused = false;
+        }
+        if (collision.collider.gameObject.CompareTag("Car"))
+        {
+            isPaused1 = false;
         }
     }
 }
